c614dfc84d91f284d5e65546209b82dc71a4caae
danascheider <dscheider@renewfinancial.com>
2016-03-27 16:50:35 -0700

    Flesh out ExampleGroup class

--- lib/rspec/example_group.rb
+++ lib/rspec/example_group.rb
@@ -1,6 +1,23 @@
 module Rambo
   module RSpec
     class ExampleGroup
+      TEMPLATE_PATH = File.expand_path('../templates/example_group_template.erb', __FILE__)
+
+      attr_reader :resource
+
+      def initialize(resource:)
+        @resource = resource
+      end
+
+      def template
+        @template ||= File.read(TEMPLATE_PATH)
+      end
+
+      def render
+        b = binding
+        ERB.new(template, 0, "", "@result").result(resource.instance_eval { b })
+        @result
+      end
     end
   end
 end
---
+++ spec/rspec/example_group_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+describe Rambo::RSpec::ExampleGroup do
+  let(:raml_file) { File.expand_path("../../support/foobar.raml", __FILE__) }
+  let(:raml)      { Raml::Parser.parse(File.read(raml_file)) }
+  let(:resource)  { raml.resources.first }
+
+  subject         { Rambo::RSpec::ExampleGroup.new(resource: resource) }
+
+  describe "#render" do
+    it "interpolates the correct values" do
+      aggregate_failures do
+        expect(subject.render).to include("describe '#{raml.resources.first.uri_partial}' do")
+        expect(subject.render).to include("describe 'GET' do")
+      end
+    end
+  end
+end
