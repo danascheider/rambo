6dcfd0bbf0b4d984bd9bae76a94173c351267cfe
danascheider <dscheider@renewfinancial.com>
2016-04-01 21:44:26 -0700

    Rewrite spec_helper_file class... seems I was drunk or something when I initially wrote

--- lib/rspec/spec_helper_file.rb
+++ lib/rspec/spec_helper_file.rb
@@ -4,12 +4,20 @@ module Rambo
 
       TEMPLATE_PATH = File.expand_path('../templates/spec_helper_file_template.erb', __FILE__)
 
-      def template
-        @template ||= File.read(TEMPLATE_PATH)
+      def initialize
+        @contents = File.read('spec/spec_helper.rb') if File.exist?('spec/spec_helper.rb')
       end
 
       def render
-        template
+        contents ? ensure_requires_present! : template
+      end
+
+      private
+
+      attr_accessor :contents
+
+      def template
+        @template ||= File.read(TEMPLATE_PATH)
       end
     end
   end
--- spec/lib/rspec/spec_helper_file_spec.rb
+++ spec/lib/rspec/spec_helper_file_spec.rb
@@ -4,17 +4,24 @@ describe Rambo::RSpec::SpecHelperFile do
 
   describe '#render' do
     context 'when there is no spec helper file' do
-      it 'calls the template' do
-        allow(File).to receive(:exist?).with('spec/spec_helper.rb').and_return(false)
+      it 'returns the template' do
+        allow(subject).to receive(:contents).and_return(nil)
         expect(subject).to receive(:template)
         subject.render
       end
     end
-  end
 
-  describe '#generate' do
-    it 'generates the spec helper file' do
-      #
+    context 'when there is a spec helper file' do
+      let(:contents) { 'require "rspec"' }
+
+      before(:each) do
+        allow(File).to receive(:read).with('spec/spec_helper.rb').and_return(contents)
+      end
+
+      it 'ensures the proper requires are present' do
+        expect_any_instance_of(described_class).to receive(:ensure_requires_present!)
+        subject.render
+      end
     end
   end
 end
