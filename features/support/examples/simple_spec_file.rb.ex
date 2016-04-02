738e0c0f8d91c93eb8f096051cae535cd4c979f9
danascheider <dscheider@renewfinancial.com>
2016-04-02 10:41:34 -0700

    Make all the cukes green, bitchez!!!

---
+++ features/support/examples/simple_spec_file.rb.ex
@@ -0,0 +1,40 @@
+require "spec_helper"
+
+describe "e-BookMobile API" do
+  include Rack::Test::Methods
+
+  describe "/authors" do
+    let(:route) { "/authors" }
+
+    describe "GET" do
+      let(:response_body) do
+        {
+          "data" => [
+            {
+              "id" => 1,
+              "first_name" => "Hermann",
+              "last_name" => "Hesse"
+            },
+            {
+              "id" => 2,
+              "first_name" => "Charles",
+              "last_name" => "Dickens"
+            }
+          ],
+          "success" => true,
+          "status" => 200
+        }.to_json
+      end
+
+      it "retrieve a list of authors" do
+        get route
+        expect(last_response.body).to eql response_body
+      end
+
+      it "returns status 200" do
+        get route
+        expect(last_response.status).to eql 200
+      end
+    end
+  end
+end
