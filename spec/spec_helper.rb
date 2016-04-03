0a6fa27d386e539d72a9271d262e7372b44ec1ef
danascheider <dscheider@renewfinancial.com>
2016-04-02 18:16:18 -0700

    Use double quotes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,20 @@
-require 'simplecov'
-require 'coveralls'
-require "json"
-require "rack/test"
+require "simplecov"
+require "coveralls"
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
-SimpleCov.start if ENV['COVERAGE']
+SimpleCov.start if ENV["COVERAGE"]
 
-require 'rspec'
-require 'rspec/core'
-require 'rspec/matchers'
-require 'rspec/expectations'
-require 'raml-rb'
+require "rspec"
+require "rspec/core"
+require "rspec/matchers"
+require "rspec/expectations"
+require "raml-rb"
 
-path = File.expand_path('../../lib', __FILE__)
+path = File.expand_path("../../lib", __FILE__)
 
 Dir.foreach(path) {|f| require f if f.match(/.*\.rb\z/) }
 
-require_relative '../lib/rspec/spec_helper_file'
+require_relative "../lib/rspec/spec_helper_file"
