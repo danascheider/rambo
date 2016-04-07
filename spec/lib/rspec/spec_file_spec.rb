67b63c124d1c1ab12a610bd6bb3bd8676c5248e1
danascheider <dscheider@renewfinancial.com>
2016-04-07 13:08:39 -0700

    Specify in context that examples apply to RAML file with example

--- spec/lib/rspec/spec_file_spec.rb
+++ spec/lib/rspec/spec_file_spec.rb
@@ -1,25 +1,28 @@
 require 'spec_helper'
 
 describe Rambo::RSpec::SpecFile do
-  let(:raml_file) { File.expand_path('../../../support/foobar.raml', __FILE__) }
   let(:raml) { Raml::Parser.parse(File.read(raml_file)) }
   let(:spec_file) { Rambo::RSpec::SpecFile.new(raml: raml) }
 
-  describe '#initialize' do
-    it 'assigns @raml' do
-      expect(spec_file.raml).to eql raml
-    end
-  end
+  context 'file with examples' do
+    let(:raml_file) { File.expand_path('../../../support/foobar.raml', __FILE__) }
 
-  describe '#template' do
-    it 'is a string' do
-      expect(spec_file.template.is_a?(String)).to be true
+    describe '#initialize' do
+      it 'assigns @raml' do
+        expect(spec_file.raml).to eql raml
+      end
     end
-  end
 
-  describe '#render' do
-    it 'interpolates the correct values' do
-      expect(spec_file.render).to include('e-BookMobile API')
+    describe '#template' do
+      it 'is a string' do
+        expect(spec_file.template.is_a?(String)).to be true
+      end
+    end
+
+    describe '#render' do
+      it 'interpolates the correct values' do
+        expect(spec_file.render).to include('e-BookMobile API')
+      end
     end
   end
 end
