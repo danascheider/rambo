5b2d67d928394d14291f68d683410c87f1062bfe
Dustin Zeisler <dustin@zeisler.net>
2016-05-20 11:17:22 -0700

    Increase test coverage slightly.

--- spec/lib/rspec/spec_helper_file_spec.rb
+++ spec/lib/rspec/spec_helper_file_spec.rb
@@ -3,7 +3,6 @@ RSpec.describe Rambo::RSpec::SpecHelperFile do
   describe "#generate" do
     context "when there is no rambo helper file" do
       it "creates the file" do
-        allow(subject).to receive(:file_already_exists?).and_return(false)
         expect(subject).to receive(:write_template)
         subject.generate
       end
