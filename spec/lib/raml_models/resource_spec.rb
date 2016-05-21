bffc100966e5ce5e400e4eff6a1227b5d3512ea4
danascheider <dscheider@renewfinancial.com>
2016-05-21 12:24:32 -0700

    Make another RSpec pass

--- spec/lib/raml_models/resource_spec.rb
+++ spec/lib/raml_models/resource_spec.rb
@@ -1,13 +1,14 @@
 RSpec.describe Rambo::RamlModels::Resource do
   let(:raml_file) { File.expand_path("../../../support/foobar.raml", __FILE__) }
   let(:raml) { Raml.parse_file(raml_file) }
+  let(:uri_partial) { raml.resources.first.first }
   let(:resource) { raml.resources.first }
 
   subject { described_class.new(resource) }
 
   describe "#to_s" do
     it "returns the URI partial" do
-      expect(subject.to_s).to eql resource.relative_uri
+      expect(subject.to_s).to eql uri_partial
     end
   end
 
