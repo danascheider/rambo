6b3db185a5977fc0e3ffa19cce915eb06a3da17c
danascheider <dscheider@renewfinancial.com>
2016-05-21 11:26:36 -0700

    Run RSpecs in random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,5 +11,10 @@ require "json_test_data"
 
 lib = File.expand_path("../../lib", __FILE__)
 SPEC_DIR_ROOT = File.expand_path("../", __FILE__)
+
 Dir.foreach("#{lib}")   {|f| require f if f.match(/.*\.rb\z/) }
-RSpec.configure { |c| c.disable_monkey_patching! }
+
+RSpec.configure do |c|
+  c.disable_monkey_patching!
+  c.order = :random
+end
