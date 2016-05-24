41a16a8ca2c88927274fd8f3fb91f04a64408d6a
danascheider <dscheider@renewfinancial.com>
2016-05-24 15:11:13 -0700

    Delete unused class

--- spec/lib/rspec/example_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-RSpec.describe Rambo::RSpec::Example do
-  let(:raml_file) { File.expand_path("../../../support/foobar.raml", __FILE__) }
-  let(:raml)      { Raml::Parser.parse_file(raml_file) }
-  let(:method)    { raml.resources.first.methods.first }
-
-  subject         { Rambo::RSpec::Example.new(resource: resource) }
-end
