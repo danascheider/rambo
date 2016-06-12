b1247c2aaca0331806ef3579ff6b38ec382edab3
danascheider <dscheider@renewfinancial.com>
2016-06-11 20:40:50 -0700

    Example for secured API feature

--- features/support/cucumber_helper.rb
+++ features/support/cucumber_helper.rb
@@ -3,7 +3,8 @@ module CucumberHelper
     possible_paths = [
       File.join(CUCUMBER_DIR_ROOT, "examples/raml/#{example}"),
       File.join(CUCUMBER_DIR_ROOT, "examples/rspec/#{example}"),
-      File.join(CUCUMBER_DIR_ROOT, "examples/rspec/#{example}")
+      File.join(CUCUMBER_DIR_ROOT, "examples/rspec/#{example}"),
+      File.join(CUCUMBER_DIR_ROOT, "examples/yaml/#{example}")
     ]
 
     possible_paths.each do |path|
