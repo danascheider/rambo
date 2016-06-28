594bb3c46e7efbe7bc81f2ca433197ad0319ddaa
danascheider <dscheider@renewfinancial.com>
2016-06-28 12:56:13 -0700

    Initial SecurityScheme class definition

--- lib/rambo/raml_models/security_scheme.rb
+++ lib/rambo/raml_models/security_scheme.rb
@@ -1,6 +1,14 @@
 module Rambo
   module RamlModels
     class SecurityScheme
+      attr_accessor :schema, :title
+      private :schema
+
+      def initialize(raml)
+        @schema, @title = raml.last.fetch("described_by", {}), raml.first
+      end
+
+      def headers
     end
   end
 end
--- spec/lib/rambo/raml_models/security_scheme_spec.rb
+++ spec/lib/rambo/raml_models/security_scheme_spec.rb
@@ -1,4 +1,8 @@
 RSpec.describe Rambo::RamlModels::SecurityScheme do
   let(:raml_file) { File.join(SPEC_DIR_ROOT, "support/secured_api.raml") }
-  let(:raml)      { Raml::Parser.parse_file(raml_file).security_schemes.auth_header }
+  let(:raml)      { Raml::Parser.parse_file(raml_file).security_schemes.first }
+
+  subject { described_class.new(raml) }
+
+  it { is_expected.to respond_to :title }
 end
